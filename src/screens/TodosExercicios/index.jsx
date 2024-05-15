fbc7a5b2ae083b348b56f76bc9e2f0f230a1301c
MatheusScaranello <matheusscaranello.564@gmail.com>
2024-05-15 17:05:27 -0300

    feat: Adicionar funcionalidade de cadastro de exercícios

--- src/screens/TodosExercicios/index.jsx
+++ src/screens/TodosExercicios/index.jsx
@@ -1,6 +1,52 @@
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
 import styles from "./styles";
+import React from "react";
+import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import apiExercicios from "../../service/Exercicios";
+import apiGruposMusculares from "../../service/GruposMusculares";
 
 export default function Cadastro() {
-  return <View style={styles.container}></View>;
+
+  const [nome, setNome] = useState("");
+  const [descricao, setDescricao] = useState("");
+  const [grupoMuscular, setGrupoMuscular] = useState("");
+  const [erro, setErro] = useState(false);
+  const [msgErro, setMsgErro] = useState("");
+  const navigation = useNavigation();
+
+  const separarGrupos = async () => {
+    try {
+      const response = await apiGruposMusculares.getAllGruposMusculares();
+      console.log(response);
+      return response;
+    } catch (error) {
+      console.error("Erro ao buscar grupos musculares:", error.message);
+      setErro(true);
+      setMsgErro("Erro ao buscar grupos musculares");
+    }
+  };
+
+  const exerciciosPorGrupo = async () => {
+    try {
+      const response = await apiExercicios.getAllExercicios(grupoMuscular);
+      console.log(response);
+      return response;
+    } catch (error) {
+      console.error("Erro ao buscar exercicios por grupo muscular:", error.message);
+      setErro(true);
+      setMsgErro("Erro ao buscar exercicios por grupo muscular");
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Todos Exercícios</Text>
+      <View style={styles.contCad}>
+        {/* fazer cards do grupo muscular */}
+
+        </View>
+      </View>
+  );
 }
+
