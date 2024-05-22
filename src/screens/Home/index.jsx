cf1923123f800c2e8bfd824b48654d5b35f45c12
marinamorais <marinamorais.564@gmail.com>
2024-05-22 17:15:31 -0300

    create: home

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 import styles from "./styles";
 import Header from "../../components/Header";
@@ -6,5 +6,8 @@ import Header from "../../components/Header";
 export default function Home() {
   return <View style={styles.container}>
     <Header />
+    <View style={styles.content}>
+          <Text style={styles.texto}>ScarFit</Text>
+    </View>
   </View>;
 }
