5c58abfa80d41ef13096d8063322367521753a61
thaynavazzoler <thayna.vazzoler@aluno.senai.br>
2024-05-27 09:11:07 -0300

    Styles.js do title

---
+++ src/components/CardSobreNos/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 32,
+        fontWeight: "bold",
+        color: "#FCD89E",
+        textAlign: "center",
+        marginBottom: 10,
+        marginTop: 30,
+    },
+});
+
+export default styles;
