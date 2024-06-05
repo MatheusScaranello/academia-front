027bb4677cde611a9abb9a28c21e3eb55b8de6da
marinamorais <marinamorais.564@gmail.com>
2024-06-05 14:49:52 -0300

    chore: Update CardCarrossel component styles

--- src/components/CardCarroseul/styles.js
+++ src/components/CardCarroseul/styles.js
@@ -3,15 +3,20 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
   slide: {
+    width: 200,
+    height: 200,
     justifyContent: "center",
     alignItems: "center",
     margin: 10,
   },
   image: {
-    width: 450,
-    height: 200,
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
   },
 });
 
