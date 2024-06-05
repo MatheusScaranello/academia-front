2ab3c8ee9d214c5a5e29ab2c8452f8f7d0bce08e
MatheusScaranello <matheusscaranello.564@gmail.com>
2024-06-05 09:56:51 -0300

    aa

--- src/routes/tab.routes.jsx
+++ src/routes/tab.routes.jsx
@@ -7,12 +7,18 @@ import Exercicio from "../screens/Exercicio";
 import Login from "../screens/Login";
 import TodosExercicios from "../screens/TodosExercicios";
 import Treino from "../screens/Treino";
+import SobreNos from "../screens/SobreNos"; // Importe a tela SobreNos
 
 const Tab = createBottomTabNavigator();
 
 const TabRoutes = () => {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen
+        name="Cadastro"
+        component={Cadastro}
+        options={{ tabBarButton: () => null }}
+      />
       <Tab.Screen
         name="Home"
         component={Home}
@@ -30,10 +36,22 @@ const TabRoutes = () => {
         }}
       />
       <Tab.Screen
-        name="Cadastro"
-        component={Cadastro}
-        options={{ tabBarButton: () => null }}
+        name="SobreNos"
+        component={SobreNos} // Adicione a nova tela aqui
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Feather
+              name="info"
+              size={24}
+              color={focused ? "#131313" : "#D6D6D6"}
+            />
+          ),
+          tabBarLabel: "Sobre Nós",
+          tabBarActiveTintColor: "#131313",
+          tabBarInactiveTintColor: "#D6D6D6",
+        }}
       />
+
       <Tab.Screen
         name="Login"
         component={Login}
