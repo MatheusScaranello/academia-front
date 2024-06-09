d65658b68dc233f97800823bc130c0f5f747778b
MatheusScaranello <matheusscaranello.564@gmail.com>
2024-06-09 15:38:36 -0300

    chore: Refactor Treino screen styles
    The styles for the Treino screen were refactored to improve readability and maintainability. The styles were moved to a separate file, "styles.js", and organized using the StyleSheet.create method. This change enhances code organization and makes it easier to manage and update the styles in the future.

--- src/screens/Treino/index.jsx
+++ src/screens/Treino/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, ScrollView, TouchableOpacity, StyleSheet, Animated, TextInput, Button } from "react-native";
 import exercicioService from "../../service/Exercicios";
+import styles from "./styles";
 
 const Treino = () => {
   const [exercicios, setExercicios] = useState([]);
@@ -164,45 +165,5 @@ const ExercicioItem = ({ exercicio, onIncrementSeries, onDecrementSeries, onRemo
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-  },
-  searchInput: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    borderRadius: 5,
-    marginBottom: 20,
-    paddingHorizontal: 10,
-  },
-  exercicioItem: {
-    marginBottom: 10,
-    padding: 10,
-    backgroundColor: '#f0f0f0',
-    borderRadius: 5,
-  },
-  exercicioNome: {
-    fontSize:  18,
-    fontWeight: 'bold',
-    marginBottom: 5,
-  },
-  exercicioInfo: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 5,
-  },
-  input: {
-    width: 50,
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    borderRadius: 5,
-    marginRight: 10,
-    paddingHorizontal: 10,
-  },
-});
 
 export default Treino;
--- src/screens/Treino/styles.js
+++ src/screens/Treino/styles.js
@@ -1,11 +1,43 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
+    container: {
+      flex: 1,
+      padding: 20,
+    },
+    searchInput: {
+      height: 40,
+      borderColor: 'gray',
+      borderWidth: 1,
+      borderRadius: 5,
+      marginBottom: 20,
+      paddingHorizontal: 10,
+    },
+    exercicioItem: {
+      marginBottom: 10,
+      padding: 10,
+      backgroundColor: '#f0f0f0',
+      borderRadius: 5,
+    },
+    exercicioNome: {
+      fontSize:  18,
+      fontWeight: 'bold',
+      marginBottom: 5,
+    },
+    exercicioInfo: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginBottom: 5,
+    },
+    input: {
+      width: 50,
+      height: 40,
+      borderColor: 'gray',
+      borderWidth: 1,
+      borderRadius: 5,
+      marginRight: 10,
+      paddingHorizontal: 10,
+    },
 });
 
 export default styles;
